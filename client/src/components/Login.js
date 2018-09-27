49aed141415c16d57a95bb9f5d4683de547f3195
Aspen James <aspenjames@tqca.org>
2018-09-26 18:36:22 -0700

    make basic login/signup forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
   render() {
     return (
-      <div></div>
+      <div className='formContainer'>
+        <div id='formErrors' />
+        <form className='login' onSubmit={this.handleOnSubmit}>
+          <label>Email: </label>
+          <input type='text' id='email' value={this.state.email}
+                 onChange={this.handleOnChange} /><br />
+          <label>Password: </label>
+          <input type='password' id='password' value={this.state.password}
+                 onChange={this.handleOnChange} /><br />
+          <input type='submit' value='Login' />
+        </form>
+      </div>
     )
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    console.log("state: ", this.state);
+    //TODO: send to api
   }
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+  state = {
+    name: '',
+    address: '',
+    email: '',
+    password: ''
+  }
+
   render() {
     return (
-      <div></div>
-    )
+      <div className='formContainer'>
+        <form className='signup' onSubmit={this.handleOnSubmit}>
+          <label>Coffee Shop Name: </label>
+          <input type='text' id='name' value={this.state.name}
+                 onChange={this.handleOnChange} /><br />
+          <label>Address: </label>
+          <input type='text' id='address' value={this.state.address}
+                 onChange={this.handleOnChange} /><br />
+          <label>Email: </label>
+          <input type='text' id='email' value={this.state.email}
+                 onChange={this.handleOnChange} /><br />
+          <label>Password: </label>
+          <input type='password' id='password' value={this.state.password}
+                 onChange={this.handleOnChange} /><br />
+          <input type='submit' value='Sign Up' />
+        </form>
+      </div>
+    );
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventSubmit;
+    console.log("state: ", this.state);
+    //TODO: send to api
   }
 }
 
