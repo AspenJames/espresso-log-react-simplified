50c1862b51cb9c344ab92654c455f68cf7d35278
Aspen James <aspenjames@tqca.org>
2018-09-26 17:11:56 -0700

    init basic Nav + login/signup components

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import { BrowserRouter as Router, Route } from 'react-router';
+import { connect } from 'react-redux';
+
+
+import NavBar from '../components/NavBar';
+import Login from '../components/Login';
+import Signup from '../components/Signup';
 
 class App extends Component {
   render() {
@@ -8,14 +15,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="App-title">Espresso.Log</h1>
+        </header> <br />
+        <Router>
+          <React.Fragment>
+            <NavBar /><br />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(App);
