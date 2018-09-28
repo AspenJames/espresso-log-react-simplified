82ef4ad3fb33b84d3fcf0780ed29e9c2a1d77fd0
Aspen James <aspenjames@tqca.org>
2018-09-28 09:39:18 -0700

    render list of coffee links

--- client/src/containers/Coffees.js
+++ client/src/containers/Coffees.js
@@ -3,17 +3,43 @@ import { connect } from 'react-redux';
 
 class CoffeesContainer extends Component {
   render() {
-    return (
-      <div>Coffees Container</div>
-    )
+    // redirect to root if not logged in
+    if (this.props.coffeeShop.id === null){
+      this.props.history.push('/');
+      return null;
+    } else {
+      return (
+        <div>{this.renderCoffees()}</div>
+        //TODO: add coffee form
+      )
+    }
   }
+
+  renderCoffees = () => {
+    if (this.props.origins.length === 0) {
+      return (
+        <div>
+          <p>You don't have any coffees in our system yet!</p>
+          <p>Add one using the form below to start tracking your espressos.</p>
+        </div>
+      );
+    } else {
+      debugger;
+      return this.props.origins.map(origin => (
+        <React.Fragment>
+          <li key={origin.id}><a href={'/coffees/' + origin.id}>{origin.name}</a></li>
+        </React.Fragment>
+      ));
+    }
+  }
+
 }
 
 const mapStateToProps = state => {
   return {
     coffeeShop: state.coffeeShop,
-    espressos: state.espressos,
-    origins: state.origins
+    espressos: state.espressos.espressos,
+    origins: state.origins.origins
   }
 }
 
