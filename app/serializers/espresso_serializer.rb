c15f77a92f85978597c0b4705dfc79ed2a3e77bd
Aspen James <aspenjames@tqca.org>
2018-09-28 13:11:49 -0700

    nest espressso routes under origin, remove unnecessary data from serializer

--- app/serializers/espresso_serializer.rb
+++ app/serializers/espresso_serializer.rb
@@ -1,5 +1,3 @@
 class EspressoSerializer < ActiveModel::Serializer
   attributes :id, :dose, :yield, :time, :notes
-  belongs_to :origin
-  has_one :coffee_shop, :through => :origin
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :coffee_shops, :only => [:show, :create, :update, :destroy]
-      resources :espressos,  :only => [:create, :update, :destroy]
-      resources :origins,  :only => [:create, :update, :destroy]
-
+      resources :origins,  :only => [:create, :update, :destroy] do
+        resources :espressos,  :only => [:index, :create, :update, :destroy]
+      end
       post '/login' => 'sessions#create'
       post '/logout' => 'sessions#destroy'
     end
