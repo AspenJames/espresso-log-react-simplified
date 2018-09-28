9cf4a2e63e9211d48d356c1fa2b4f4fd8858e557
Aspen James <aspenjames@tqca.org>
2018-09-28 13:16:26 -0700

    use thunk to fetch espresso data

--- client/src/actions/espressosActions.js
+++ client/src/actions/espressosActions.js
@@ -1,1 +1,9 @@
-export const addEspresso = espresso => ({type: "ADD_ESPRESSO", espresso});
+export const fetchEspressos = originId => {
+  return dispatch => {
+    dispatch({type: "LOADING_ESPRESSOS"});
+    return fetch(`/api/v1/origins/${originId}/espressos`)
+      .then(resp => resp.json())
+      .then(json => json.espressos)
+      .then(espressos => dispatch({type: "FETCH_ESPRESSOS", espressos}));
+  }
+}
